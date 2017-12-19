5e0e85017a0997debbf77da151b8d11fae410a2c
Nattapon Chitlaiad <imagine_joe@hotmail.com>
2017-12-19 15:06:50 +0700

    init

--- ICT13580028B/ICT13580028B/App.xaml.cs
+++ ICT13580028B/ICT13580028B/App.xaml.cs
@@ -12,8 +12,26 @@ namespace ICT13580028B
         public App()
         {
             InitializeComponent();
+            var np = new NavigationPage(new NavPage1());
 
-            MainPage = new ICT13580028B.MainPage();
+            var tp = new TabbedPage();
+            tp.Children.Add(new TabPage1());
+            tp.Children.Add(new TabPage2());
+            tp.Children.Add(new TabPage3());
+
+
+
+            var cp = new CarouselPage();
+            cp.Children.Add(new TabPage1());
+            cp.Children.Add(new TabPage2());
+            cp.Children.Add(new TabPage3());
+
+
+            var mp = new MasterDetailPage();
+            mp.Master = new MenuPage();
+            mp.Detail = new NavigationPage(new ICT13580028B.MainPage());
+
+            MainPage = mp;
         }
 
         protected override void OnStart()
